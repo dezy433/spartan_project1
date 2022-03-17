78278338be3d276364457721497987d7e46f4369
derenertim@hotmail.com <derenertim@hotmail.com>
2022-03-17 23:51:32 +0000

    can follow the video and the principle of it but struggling to add the functions to the api

--- main.py
+++ main.py
@@ -10,18 +10,16 @@ def home_page():
     description = (welcome+"\n"+tutorial)
     return description
 #method: POST, route: /spartan/remove?id=sparta_id
+
+
 @flask_object.route("/spartan/<spartan_id>", methods = ["GET"])
 def employee_getter(id_vars):
-    id_var = request.args.get("ID")
-    first_name_var = request.args.get("Fist name: ")
-    last_name_var = request.args.get("Last name: ")
-    birth_year_var = request.args.get("Birth Year: ")
-    birth_month_var = request.args.get("Birth month: ")
-    birth_day_var = request.args.get("Birth Day: ")
-    s_course_var = request.args.get("Course: ")
-    s_stream_var = request.args.get("Stream: ")
+    data = jsonify(management.load_from_json() )
+    return data
+@flask_object.route("/spartan",methods = ["GET"])
+def sparta_list():
 
-    return f"{id_vars}"
+    management.load_from_json()
 #  f"Welcome back : {first_name_var}\n Here are you details:\nID: {id_var}\nFirst name: {first_name_var}\n Last name: {last_name_var}\n Birth Year:{birth_year_var}, Birth month:{birth_month_var}\nBirth day: {birth_day_var}\nCourse: {s_course_var}\nStream: {s_stream_var}  "
 
 if __name__ == "__main__":
